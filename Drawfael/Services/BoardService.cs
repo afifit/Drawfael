6fe1ecddc8b873d20c68af28271f28f4ab5e93e6
dorcohen <dorafifitcohen@gmail.com>
2022-04-09 10:03:06 +0300

    fixed island coloring

--- Drawfael/Services/BoardService.cs
+++ Drawfael/Services/BoardService.cs
@@ -31,7 +31,7 @@ namespace Drawfael.Services
 
         public async Task<Cell> GetCell(int x, int y)
         {
-            if(x < 0 || y < 0 || x > Board.SIZE || y> Board.SIZE)
+            if (x < 0 || y < 0 || x > Board.SIZE || y > Board.SIZE)
             {
                 return null;
             }
@@ -60,44 +60,22 @@ namespace Drawfael.Services
             //CheckBoard(CellColor.Blue);
             CheckBoard(CellColor.Green);
             //CheckBoard(CellColor.Yellow);
-        } 
+        }
         private void CheckBoard(CellColor color)
         {
-            void dfs(Cell[,] matrix, bool[,] visited, int x,
-                    int y, int n, int m, CellColor islandColor)
-            {
-
-                // If the land is already visited
-                // or there is no land or the
-                // coordinates gone out of matrix
-                // break function as there
-                // will be no islands
-                if (x < 0 || y < 0 || x >= n || y >= m
-                    || visited[x, y] == true || matrix[x, y].Color != islandColor)
-                    return;
-
-                // Mark land as visited
-                visited[x, y] = true;
-
-
-                // Traverse to all adjacent elements
-                dfs(matrix, visited, x + 1, y, n, m, islandColor);
-                dfs(matrix, visited, x, y + 1, n, m, islandColor);
-                dfs(matrix, visited, x - 1, y, n, m, islandColor);
-                dfs(matrix, visited, x, y - 1, n, m, islandColor);
-            }
 
             bool dfs_fill(Cell[,] matrix, bool[,] visited, bool[,] island, int x,
                 int y, int n, int m, CellColor islandColor)
             {
 
                 //if the island reached the edge - then it is not enclosed inside
-                if (x < 0 || y < 0 || x >= n || y >= m) return false;
+                if (x < 0 || y < 0 || x >= n || y >= m)
+                    return false;
 
-                if (visited[x, y] == true || matrix[x, y].Color == islandColor)
+                if (island[x, y] == true || matrix[x, y].Color == islandColor)
                     return true;
 
-                // Mark land as visited
+                // Mark land as visited and as an island
                 visited[x, y] = true;
                 island[x, y] = true;
 
@@ -109,7 +87,7 @@ namespace Drawfael.Services
                 dfs_fill(matrix, visited, island, x, y - 1, n, m, islandColor);
             }
             // Function that counts the closed island
-            int colorIslands(Cell[,] matrix, int n,
+            void colorIslands(Cell[,] matrix, int n,
                                          int m, CellColor islandColor)
             {
 
@@ -127,46 +105,20 @@ namespace Drawfael.Services
                     for (int j = 0; j < m; ++j)
                     {
 
-                        // Traverse corners
-                        if ((i * j == 0 || i == n - 1 || j == m - 1)
-                            && matrix[i, j].Color == islandColor
-                            && visited[i, j] == false)
-                            dfs(matrix, visited, i, j, n, m, islandColor);
-                    }
-                }
-
-                // To stores number of closed islands
-                int result = 0;
-
-                for (int i = 0; i < n; ++i)
-                {
-                    for (int j = 0; j < m; ++j)
-                    {
-
-                        // If the land not visited
-                        // then there will be atleast
-                        // one closed island
-                        if (visited[i, j] == false
-                            && matrix[i, j].Color == islandColor)
+                        // traverse only unvisited nodes and "water"
+                        if (matrix[i, j].Color != islandColor&& visited[i, j] == false)
                         {
-                            result++;
 
-                            // Mark all lands associated
-                            // with island visited.
-                            dfs(matrix, visited, i, j, n, m, islandColor);
-
-                            //fill inner - define inner as button right?
                             var island = new bool[n, m];
-                            var isIsland = dfs_fill(matrix, visited, island, i + 1, j + 1, n, m, islandColor);
-                            if (isIsland)
+                            if (dfs_fill(matrix, visited, island, i, j, n, m, islandColor))
                             {
                                 for (int k = 0; k < n; ++k)
                                 {
-                                    for (int l = 0; l < m; ++l)
+                                    for (var l = 0; l < m; ++l)
                                     {
                                         if (island[k, l])
                                         {
-                                            ChangeCell(k, l, color);
+                                            ChangeCell(k, l, islandColor);
                                         }
                                     }
                                 }
@@ -175,11 +127,9 @@ namespace Drawfael.Services
                     }
                 }
 
-                // Return the readonly count
-                return result;
             }
 
-             colorIslands(Board.Cells, Board.SIZE, Board.SIZE, color);
+            colorIslands(Board.Cells, Board.SIZE, Board.SIZE, color);
         }
     }
 }
